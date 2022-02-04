f24520aab17a6b9a90c5cd105051536f3b3d131f
Miguel Acosta <migue.un.av@gmail.com>
2022-02-04 16:16:48 -0600

    Se borra la carpeta de target

--- src/test/java/com/cuenca/CuencaClientTest.java
+++ src/test/java/com/cuenca/CuencaClientTest.java
@@ -9,7 +9,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Base64;
 import java.util.Optional;
 
 import static org.mockito.Mockito.mock;
